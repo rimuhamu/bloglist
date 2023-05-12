3f9ccb2b7eac87817c2b4352bc5db51b321289f6
Emir Risyad <emirrsyd@gmail.com>
2023-05-12 23:25:37 +0200

    4.4 done

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,5 +1,11 @@
 const dummy = (blogs) => 1;
 
+const totalLikes = (blogs) => {
+  const sumLikes = blogs.reduce((sum, blog) => sum + blog.likes, 0);
+  return sumLikes;
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
