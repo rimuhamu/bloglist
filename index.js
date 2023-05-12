53c3b357c13cfb4b9151ca6cb2674475292d7cba
Emir Risyad <emirrsyd@gmail.com>
2023-05-12 13:36:57 +0200

    4.1 done

--- index.js
+++ index.js
@@ -1,42 +1,57 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
 
 const blogSchema = new mongoose.Schema({
   title: String,
   author: String,
   url: String,
-  likes: Number
-})
+  likes: Number,
+});
 
-const Blog = mongoose.model('Blog', blogSchema)
+blogSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
 
-const mongoUrl = 'mongodb://localhost/bloglist'
-mongoose.connect(mongoUrl)
+const Blog = mongoose.model("Blog", blogSchema);
 
-app.use(cors())
-app.use(express.json())
+const mongoUrl = process.env.MONGO_URI;
+mongoose.connect(mongoUrl);
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
+app.use(cors());
+app.use(express.json());
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
+app.get("/api/blogs", (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs);
+  });
+});
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.get("/api/blogs/:id", (request, response) => {
+  Blog.findById(request.params.id).then((blogs) => {
+    if (blogs) {
+      response.json(blogs);
+    } else {
+      response.status(404).end();
+    }
+  });
+});
 
-const PORT = 3003
+app.post("/api/blogs", (request, response) => {
+  const blog = new Blog(request.body);
+
+  blog.save().then((result) => {
+    response.status(201).json(result);
+  });
+});
+
+const PORT = 3003;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
