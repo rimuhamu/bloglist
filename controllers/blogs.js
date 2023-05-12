6eecb4b0719dc98c2b1ab5a22409080975b9e258
Emir Risyad <emirrsyd@gmail.com>
2023-05-12 22:43:42 +0200

    4.3 done

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,28 +1,33 @@
-const blogsRouter = require("express").Router();
-const Blog = require("../models/blog");
+const blogsRouter = require('express').Router();
+const Blog = require('../models/blog');
 
-blogsRouter.get("/", (request, response) => {
+blogsRouter.get('/', (request, response) => {
   Blog.find({}).then((blogs) => {
     response.json(blogs);
   });
 });
 
-blogsRouter.get("/:id", (request, response, next) => {
-  Blog.findById(request.params.id).then((blogs) => {
-    if (blogs) {
-      response.json(blogs);
-    } else {
-      response.status(404).end();
-    }
-  }).catch(error => next(error))
+blogsRouter.get('/:id', (request, response, next) => {
+  Blog.findById(request.params.id)
+    .then((blogs) => {
+      if (blogs) {
+        response.json(blogs);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
-blogsRouter.post("/", (request, response, next) => {
+blogsRouter.post('/', (request, response, next) => {
   const blog = new Blog(request.body);
 
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  }).catch(error => next(error))
+  blog
+    .save()
+    .then((result) => {
+      response.status(201).json(result);
+    })
+    .catch((error) => next(error));
 });
 
-module.exports = blogsRouter
+module.exports = blogsRouter;
