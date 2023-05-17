f9ff5143001efac6ed639d6ea6f6691df7c5c33d
Emir Risyad <emirrsyd@gmail.com>
2023-05-18 00:13:04 +0200

    4.15 done

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const blogsRouter = require("./controllers/blogs");
 const middleware = require("./utils/middleware");
 const logger = require("./utils/logger");
 const config = require("./utils/config");
+const usersRouter = require("./controllers/users");
 
 mongoose.set("strictQuery", false);
 
@@ -25,6 +26,7 @@ app.use(cors());
 app.use(express.json());
 app.use(middleware.requestLogger);
 app.use("/api/blogs", blogsRouter);
+app.use("/api/users", usersRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
